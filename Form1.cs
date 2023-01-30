8c45f8e4112529ae57da90770f7911790fbc0a72
Nikolay <nikolay.zlatev2005@gmail.com>
2023-01-30 17:06:01 +0200

    make the code more readable, adding comments and adding two methods: SqlCommand and Select

--- Form1.cs
+++ Form1.cs
@@ -11,56 +11,78 @@ namespace Expense_Tracker
         {
             InitializeComponent();
         }
-        
-        private void button1_Click(object sender, EventArgs e)
+        //make the connection
+        //connectionString to my db(ExpenseTracker)
+        SqlConnection con = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ExpenseTracker;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+        //method for sql crud operations(without select) and updating the db
+        public void SqlCommand(string querry, SqlConnection con)
         {
-            //make the connection
-            string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ExpenseTracker;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-            SqlConnection con = new SqlConnection(connectionString);
-            //open the connection
-            con.Open();
-            //make the insert query
-            string insert = $"INSERT INTO Information(Type, Name, Date, Amount) VALUES ({type_comboBox.Text}, {date_txt.Text}, {name_txt.Text}, {decimal.Parse(amount_txt.Text)})";
-            SqlCommand addingCommand = new SqlCommand(insert, con);
+            SqlCommand querryCommand = new SqlCommand(querry, con);
+
             //adding elements
-            addingCommand.ExecuteNonQuery();
-            //close the connection
-            con.Close();
+            querryCommand.ExecuteNonQuery();
+
+            //SqlDataAdapter - works as a bridge between DataTable and Server
+            //It is used to fill the DataTable and update the data source
+            SqlDataAdapter da = new SqlDataAdapter(querryCommand);
+            DataTable dataTable = new DataTable();
+            da.Fill(dataTable);
+            dataGridView1.DataSource = dataTable;
+        }
+        //select method
+        public void Select()
+        {
+            //make the select query
             string select = "SELECT * FROM Information";
             SqlCommand selecingCommand = new SqlCommand(select, con);
+
+            //fill the DataTable and update the data source
             SqlDataAdapter da = new SqlDataAdapter(selecingCommand);
             DataTable dataTable = new DataTable();
             da.Fill(dataTable);
             dataGridView1.DataSource = dataTable;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //opening the connection
+            con.Open();
+
+            //make the insert query
+            string insert = $"INSERT INTO Information(Type, Name, Date, Amount) VALUES ({type_comboBox.Text}, {date_txt.Text}, {name_txt.Text}, {decimal.Parse(amount_txt.Text)})";
+            
+            //insert
+            SqlCommand(insert,con);
+
+            //closing the connection
+            con.Close();
+
+            //selecting the table automatically after we insert an element
+            Select(); 
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ExpenseTracker;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-            SqlConnection con = new SqlConnection(connectionString);
-            string select = "SELECT * FROM Information";
-            SqlCommand selecingCommand = new SqlCommand(select, con);
-            SqlDataAdapter da = new SqlDataAdapter(selecingCommand);
-            DataTable dataTable = new DataTable();
-            da.Fill(dataTable);
-            dataGridView1.DataSource = dataTable;
+            //selecting the table
+            Select();
         }
 
         private void btnDelete_Click(object sender, EventArgs e)
         {
-            string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ExpenseTracker;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-            SqlConnection con = new SqlConnection(connectionString);
+            //opening the connection
             con.Open();
+
+            //make the insert query
             string delete = $"delete FROM Information where id={int.Parse(txtDelete.Text)}";
-            SqlCommand deletingCommand = new SqlCommand(delete, con);
-            deletingCommand.ExecuteNonQuery();
+
+            //delete
+            SqlCommand(delete, con);
+
+            //closing the connection
             con.Close();
-            string select = "SELECT * FROM Information";
-            SqlCommand selecingCommand = new SqlCommand(select, con);
-            SqlDataAdapter da = new SqlDataAdapter(selecingCommand);
-            DataTable dataTable = new DataTable();
-            da.Fill(dataTable);
-            dataGridView1.DataSource = dataTable;
+
+            //select after deleting
+            Select();
         }
     }
 }
